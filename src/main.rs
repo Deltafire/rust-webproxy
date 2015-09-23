e518fe6246182ac892c3287a1bbafa628fd433d2
Christopher O'Neill <code@chrisoneill.co.uk>
2015-09-23 11:09:23 +0100

    Now works with a web browser.

--- src/main.rs
+++ src/main.rs
@@ -4,59 +4,49 @@ use std::thread;
 use std::io::{copy, BufReader, Write, BufRead};
 //use std::io::prelude::*;
 
-fn handle_client(mut source: TcpStream) {
+fn handle_client(source: TcpStream) {
     // Read & parse GET request
     println!("Client connected: {}", source.peer_addr().unwrap());
     let mut source_clone = source.try_clone().unwrap();
-    let mut connect_string = String::new();
-    {
-        let source = source.try_clone().unwrap();
-        let mut reader = BufReader::new(source);
-        let bytes_read = reader.read_line(&mut connect_string).unwrap();
-        println!("Read {} bytes:\n{}\n", bytes_read, &connect_string);
-    }
-    let host = parse_connect(&connect_string).unwrap();
-
+    let mut reader = BufReader::new(source);
+    let mut line = String::new();
+    reader.read_line(&mut line).unwrap();
+    println!("{}", line.trim_right());
+    let args = line.split_whitespace().collect::<Vec<_>>();
+    let command = args[0].to_uppercase();
+    let host = match command.as_ref() {
+        "CONNECT" => args[1],
+        _ => args[1].split('/').collect::<Vec<_>>()[2],
+    }.to_string();
+    let host_port = match host.find(':') {
+        Some(_) => host,
+        None => host + ":80",
+    };
+    println!("Host: {}", host_port);
     // Connect to remote server
-    let mut dest = TcpStream::connect(&host).unwrap();
+    let mut dest = TcpStream::connect(&host_port as &str).unwrap();
+    if command.eq("CONNECT") {
+        // Consume empty line
+        let mut line = String::new();
+        while line.ne("\0x0d\0x0a") {
+            line.clear();
+            if reader.read_line(&mut line).unwrap() == 0 { break }
+        }
+        let _ = source_clone.write(b"200 Connected\r\n");
+    } else {
+        let _ = dest.write(line.as_ref());
+    }
     let mut dest_clone = dest.try_clone().unwrap();
-
-    let _ = dest.write(&connect_string.as_bytes()).unwrap();
 
     thread::spawn(move|| {
         let _ = copy(&mut dest_clone, &mut source_clone);
         let _ = source_clone.shutdown(Shutdown::Both);
         });
-    let _ = copy(&mut source, &mut dest);
+    let _ = copy(&mut reader, &mut dest);
 
-    println!("Client disconnecting: {}", dest.peer_addr().unwrap());
+    println!("Client disconnected: {}", dest.peer_addr().unwrap());
     let _ = dest.shutdown(Shutdown::Both);
 }
-
-fn parse_connect(connect_string: &String) -> Result<&str, String> {
-    // Format is "CONNECT host:port HTTP/1.1"
-    println!("Parsing {}", &connect_string);
-    let words: Vec<&str> = connect_string.split_whitespace().collect();
-    if words[0] != "CONNECT" && words[2] != "HTTP/1.1" {
-        return Err("Parse error: ".to_string() + &connect_string);
-    }
-    return Ok(words[1]);
-}
-
-#[test]
-fn test_parse_connect() {
-    assert_eq!(
-        "test.com:888",
-        parse_connect(&"CONNECT test.com:888 HTTP:/1.1".to_string()).unwrap())
-}
-
-#[test]
-#[should_panic]
-fn test_panic_parse_connect() {
-    let _ = parse_connect(&"blah blah".to_string());
-}
-
-
 
 fn main() {
     // Config
